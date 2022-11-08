c54c4c52b60875af8bb1397af05e4a3bc53bd00a
tiendung20 <ntdung2316@gmail.com>
2022-11-08 16:38:14 +0700

    add CSS

--- components/ToDoForm.vue
+++ components/ToDoForm.vue
@@ -1,14 +1,19 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <label for="new-todo-input"> What needs to be done? </label>
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg">
+        What needs to be done?
+      </label>
+    </h2>
     <input
       id="new-todo-input"
       v-model.lazy.trim="label"
       type="text"
       name="new-todo"
       autocomplete="off"
+      class="input__lg"
     />
-    <button type="submit">Add</button>
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
   </form>
 </template>
 
